6c15a0062f3799ebb3e4ecb5260e1c66217298e3
HadrienRenaudLebret <hadrien.renaudlebret@gmail.com>
2017-02-09 11:53:25 +0100

    Adding variable gestion.

--- main.py
+++ main.py
@@ -14,13 +14,30 @@ import fileio as fio
 from neuralnet import NeuralNetwork
 
 
+# *********************************** Data ************************************
+# Data :
+
+learning_sample_folder = 'LearningSample'
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,?;.:!éàè'()+-\"="
+
+length_alphabet = 8
+
+
 # ******************************************** Argparse ********************************************
 
 def argparsor():
     """Return the parser for the programm."""
     parser = argparse.ArgumentParser(description="Show a deep learning example.")
+
+    parser.add_argument("--learning_directory", default=learning_sample_folder,
+                        help="Directory in which the samples are given")
+    parser.add_argument("--length_alphabet", type=int,
+                        help="Shortcut for alphabet.")
+    parser.add_argument("--alphabet", help="Alphabet considered.")
+
     sub_parsers = parser.add_subparsers(dest='commands')
-    parser.set_defaults(func=parser.print_usage)
+    parser.set_defaults(func=create_arg_default_function(parser))
 
     parser_learn = sub_parsers.add_parser("learn")
     parser_learn.set_defaults(func=arg_learn)
@@ -28,16 +45,18 @@ def argparsor():
     return parser
 
 
-def arg_default_function():
-    """Default function, do nothing."""
-    pass
+def create_arg_default_function(parser):
+    """Create a default function, print usage."""
+    def print_usage_bis(*args, **kwargs):
+        parser.print_usage()
+    return print_usage_bis
 
 
-def arg_learn():
+def arg_learn(directory, alphabet):
     """Action to execute on argument learn."""
-    neur = NeuralNetwork("400:150:50:" + str(fio.length_alphabet))
+    neur = NeuralNetwork("400:150:50:" + str(length_alphabet))
     neur.randomize_factors()
-    print(fio.learn_on_folder(neur, limit_repet=250))
+    print(fio.learn_on_folder(neur, learning_sample_folder, alphabet, limit_repet=250))
 
 
 # **************************************** Excecutable code ****************************************
@@ -45,4 +64,9 @@ def arg_learn():
 
 if __name__ == '__main__':
     args = argparsor().parse_args()
-    args.func()
+    if alphabet in args:
+        args.func(args.learning_directory, args.alphabet)
+    elif length_alphabet in args:
+        args.func(args.learning_directory, alphabet[:args.length_alphabet])
+    else:
+        args.func(args.learning_directory, alphabet[:length_alphabet])
